e54860f73a04252f9493616242e267627bc6b907
Guilherme Samuel <guisamuel800@gmail.com>
2025-09-28 16:49:29 -0300

    fix: change postmapping to 'orquestrador' and 'usuarios' microsservices find route of patients

--- graacc_api_usuarios/src/main/java/org/codelab/graacc/Usuarios/integration/PatientClient.java
+++ graacc_api_usuarios/src/main/java/org/codelab/graacc/Usuarios/integration/PatientClient.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 )
 public interface PatientClient {
 
-    @PostMapping("/pesquisar")
+    @PostMapping("/pacientes/pesquisar")
     PatientDTO pesquisar(@RequestBody PatientRequestDTO patientRequestDTO);
 }
