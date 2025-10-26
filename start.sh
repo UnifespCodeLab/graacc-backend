735f3f629dc0f600d18d6b00bd054d97ee9b6cd2
Guilherme Samuel <guisamuel800@gmail.com>
2025-10-26 11:48:32 -0300

    chore: update script and docker-container file to support fully local-network

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Script para construir e iniciar todos os serviços do projeto com Docker Compose.
+echo "Atualizando todos os microsserviços para o último commit..."
+git submodule foreach git pull origin main
+
+echo "Criando a rede local-network..."
+docker network create local-network
 
 echo "Iniciando todos os serviços em modo detached..."
 docker-compose up --build -d
